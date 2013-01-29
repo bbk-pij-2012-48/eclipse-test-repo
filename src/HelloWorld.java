33ed94641268b3bc6252fc40bc7f827e28dc671b
RBennett <rbennett485gmail>
2013-01-29 16:45:21 +0000

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	/**
+	 * Standard Hello, world! program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
